cb3149c32fb51a079df9f3b3be1d02e76ad47607
Gabriela Trevisani <eugabrielat@gmail.com>
2024-11-10 09:48:26 -0300

    fix: grid align :cactus:

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
+
 from kivy.lang import Builder
 
 from kivy.uix.boxlayout import BoxLayout
@@ -27,134 +28,130 @@ KV = '''
     orientation: 'vertical'
 
     MDTextField:
-
         id: input_field
-
         hint_text: "Insira um número"
-
         helper_text_mode: "on_focus"
-
         input_filter: "float"
+        text_color: "black"
+
 
     GridLayout:
-
         cols: 4
-
         spacing: dp(10)
-
+        size_hint: .5,.5
+        pos_hint: {'center_x':.5,'center_y':.5}
+    
         MDRaisedButton:
 
             text: "1"
+            id: btn
             md_bg_color: "#edfff3"
-                        
+            text_color: "black"
             on_press: app.on_number_press(1)
 
         MDRaisedButton:
 
             text: "2"
             md_bg_color: "#dcffe7"
-
-
+            text_color: "black"
             on_press: app.on_number_press(2)
 
         MDRaisedButton:
 
             text: "3"
             md_bg_color: "#caffdb"
-
+            text_color: "black"
             on_press: app.on_number_press(3)
 
         MDRaisedButton:
 
             text: "+"
             md_bg_color: "#b9ffcf"
-
+            text_color: "black"
             on_press: app.on_operator_press("+")
 
         MDRaisedButton:
 
             text: "4"
             md_bg_color: "#ffebe8"
-
+            text_color: "black"                       
             on_press: app.on_number_press(4)
 
         MDRaisedButton:
 
             text: "5"
             md_bg_color: "#ffd7d0"
-
+            text_color: "black"
             on_press: app.on_number_press(5)
 
         MDRaisedButton:
 
             text: "6"
             md_bg_color: "#ffc3b9"
-
+            text_color: "black"           
             on_press: app.on_number_press(6)
 
         MDRaisedButton:
 
             text: "-"
             md_bg_color: "#ffafa2"
-
+            text_color: "black"
             on_press: app.on_operator_press("-")
 
         MDRaisedButton:
 
             text: "7"
             md_bg_color: "#feffd5"
-
+            text_color: "black"           
             on_press: app.on_number_press(7)
 
         MDRaisedButton:
 
             text: "8"
-            id: btn
             md_bg_color: "#fdffb3"
-
+            text_color: "black"
             on_press: app.on_number_press(8)
 
         MDRaisedButton:
 
             text: "9"
             md_bg_color: "#fcff90"
-
+            text_color: "black"           
             on_press: app.on_number_press(9)
 
         MDRaisedButton:
 
             text: "*"
             md_bg_color: "#fbff6e"
-
+            text_color: "black"
             on_press: app.on_operator_press("*")
 
         MDRaisedButton:
 
             text: "C"
             md_bg_color: "#dfdff4"
-
+            text_color: "black"
             on_press: app.clear_input()
 
         MDRaisedButton:
 
             text: "0"
             md_bg_color: "#d1d1ea"
-
+            text_color: "black"
             on_press: app.on_number_press(0)
 
         MDRaisedButton:
 
             text: "="
             md_bg_color: "#c2c2df"
-
+            text_color: "black"
             on_press: app.calculate_result()
 
         MDRaisedButton:
 
             text: "/"
             md_bg_color: "#b3b4d5"
-    
-
+            text_color: "black"
             on_press: app.on_operator_press("/")
 
 '''
@@ -166,18 +163,17 @@ class CalculatorApp(BoxLayout):
         current_text = self.ids.input_field.text
         new_text = f"{current_text}{number}"
         self.ids.input_field.text = new_text
-        self.ids.btn.text_color = "black"
-        self.ids.btn.md_bg_color = "white"
+
     def on_operator_press(self, operator):
 
         current_text = self.ids.input_field.text
-
         new_text = f"{current_text} {operator}"
         self.ids.input_field.text = new_text
 
     def clear_input(self):
 
         self.ids.input_field.text = ""
+
     def calculate_result(self):
 
         try:
@@ -189,6 +185,7 @@ class CalculatorApp(BoxLayout):
         except Exception as e:
 
             self.ids.input_field.text = e
+
 class CalculatorMDApp(MDApp):
 
     def build(self):
