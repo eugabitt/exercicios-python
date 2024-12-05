571f3277c5a77a84356a1d5c373a0e4296689621
Gabriela Trevisani <eugabrielat@gmail.com>
2024-12-04 21:25:22 -0300

    fix: relatório final 📝

--- Calc_Media.py
+++ Calc_Media.py
@@ -1,16 +1,36 @@
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-nota3 = float(input("Digite a terceira nota: "))
- 
-def calcular_media(notas):
-    total = sum(notas)
-    media = total / len(notas)
-    return media
+notas = list()
+loop_exit = False
+
+while not loop_exit:
+    teste = input("Digite a nota ou aperte X para sair: ")
+    if teste == 'x' or teste == 'X':
+        loop_exit = True
+        break
+    notas.append(float(teste))
+
+def calcular_media(notas_calc):
+    total = sum(notas_calc)
+    media_calc = total / len(notas_calc)
+    return media_calc
 
 arredondar_media = lambda media: round(media, 0)
-media = calcular_media([nota1, nota2, nota3])
+media = calcular_media(notas)
 media_arredondada = arredondar_media(media)
+
+
 if media_arredondada >= 7:
-    print("Aprovada(o) com média", media_arredondada,".")
+    situacao = "Aprovada(o)"
+else: 
+    situcao_n = "Reprovada(o)"
+
+for teste in notas: 
+    print("A nota do(a) aluno(a) é: ", teste)
+
+print("A média do(a) aluno(a) é: ", media_arredondada)    
+
+if media_arredondada >= 7:
+    print("O/a aluno(a) foi: ",situacao)
 else:
-    print("Reprovada(o)")
+    print("O/a aluno(a) foi: ",situcao_n)
+
+print("Mais bonitnho")
