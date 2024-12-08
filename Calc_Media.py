c3b919a335928548750090edf7f85fa1bd5125f1
Gabriela Trevisani <eugabrielat@gmail.com>
2024-12-07 22:51:57 -0300

    fix: comentários 🐬

--- Calc_Media.py
+++ Calc_Media.py
@@ -1,36 +1,37 @@
-notas = list()
-loop_exit = False
 
-while not loop_exit:
+notas = list() # guardando as notas em lista
+loop_exit = False 
+
+# enquanto o usuário não sair do loop teclando X, será solicitado as notas
+while not loop_exit: 
     teste = input("Digite a nota ou aperte X para sair: ")
     if teste == 'x' or teste == 'X':
         loop_exit = True
-        break
+        break # para se digitado X
     notas.append(float(teste))
 
+# função para calcular a média
 def calcular_media(notas_calc):
     total = sum(notas_calc)
     media_calc = total / len(notas_calc)
     return media_calc
+    
+# função lambda para arredondar a média 
+arredondar_media = lambda media: round(media, 0) 
 
-arredondar_media = lambda media: round(media, 0)
 media = calcular_media(notas)
 media_arredondada = arredondar_media(media)
 
-
+# define a situação do aluno
 if media_arredondada >= 7:
     situacao = "Aprovada(o)"
-else: 
-    situcao_n = "Reprovada(o)"
+else:
+    situacao = "Reprovada(o)"
 
-for teste in notas: 
+# exibe relatório final 
+for teste in notas:
     print("A nota do(a) aluno(a) é: ", teste)
 
-print("A média do(a) aluno(a) é: ", media_arredondada)    
+print("A média do(a) aluno(a) é: ", media_arredondada)
 
-if media_arredondada >= 7:
-    print("O/a aluno(a) foi: ",situacao)
-else:
-    print("O/a aluno(a) foi: ",situcao_n)
-
-print("Mais bonitnho")
+print("O/a aluno(a) foi: ",situacao)
